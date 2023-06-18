17c42c1c84d8fc7ea26109dcc9d958c4167117b2
nagaraj6618 <nagaraj516700@gmail.com>
2023-06-18 08:31:22 +0530

    Added vercel.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 3500;
+const PORT = process.env.PORT || 3500;
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
